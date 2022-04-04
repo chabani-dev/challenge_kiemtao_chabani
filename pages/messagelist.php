58e05f68ff1fcee27433af2cc804e603d7ac9e60
chabani-dev <zerkachabani2021@gmail.com>
2022-04-04 12:37:38 +0200

    modifiersfichiers

--- pages/messagelist.php
+++ pages/messagelist.php
@@ -6,7 +6,7 @@
     <em>ajouter&nbsp;un&nbsp;message</em></a></h2>
     <ul id="messages-list">
         <li class="message">
-            <h3 title="<?=$value?>">Philibert</h3>
+            <h3 title="animaux: réincarnation, teflon">Philibert</h3>
             <p>Prems !</p>
             <h4 class="date">30/03/2022</h4>
         </li>
